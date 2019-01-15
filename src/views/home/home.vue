0f41535a5bea1d8e0da8cecd27d58e49c24ac66d
blind181 <blind181@126.com>
2019-01-15 16:56:31 +0800

    copyright

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -5,28 +5,63 @@
     <div class="top-banner">
       <div style="width:1200px;height:180px;margin:0 auto;align-items:center;justify-content:center;display:flex;">
 
-        <img src="./../../images/xc_l.png" style="width:300px;">
-        <img src="./../../images/logo_r.png" style="width:150px;margin:15px 0px 10px 450px;">
+        <img
+          src="./../../images/xc_l.png"
+          style="width:300px;"
+        >
+        <img
+          src="./../../images/logo_r.png"
+          style="width:150px;margin:15px 0px 10px 450px;"
+        >
       </div>
     </div>
     <div class="main-container">
       <Row class="subject-choose">
-        <Col span="12" class="subject-item" :class="choosedSubject==1&&'choosed'">
+        <Col
+          span="12"
+          class="subject-item"
+          :class="choosedSubject==1&&'choosed'"
+        >
         <div @click="changeSubject(1)">
           科目一
         </div>
         </Col>
-        <Col span="12" class="subject-item" :class="choosedSubject==4&&'choosed'">
+        <Col
+          span="12"
+          class="subject-item"
+          :class="choosedSubject==4&&'choosed'"
+        >
         <div @click="changeSubject(4)">
           科目四
         </div>
         </Col>
       </Row>
       <Row>
-        <Col span="6" v-for="(item,index) in subList" :key="index" class="subject-warapper">
-        <subject-item :bgClass="item.bgClass" :subjectName="item.subjectName" :choosedSubject="choosedSubject"></subject-item>
+        <Col
+          span="6"
+          v-for="(item,index) in subList"
+          :key="index"
+          class="subject-warapper"
+        >
+        <subject-item
+          :bgClass="item.bgClass"
+          :subjectName="item.subjectName"
+          :choosedSubject="choosedSubject"
+        ></subject-item>
         </Col>
       </Row>
+    </div>
+    <div class="footer">
+      <div class="copyright">版权所有 ICP证：粤ICP备18145529号-1</div>
+      <div> <a
+          target="_blank"
+          href="http://www.beian.gov.cn/portal/registerSystemInfo?spm=5176.8087400.631162.108.31fe15c96UGiFY&recordcode=44150202000124"
+          style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"
+        ><img
+            style="float: left; width: 20px;"
+            src='./../../images/police.png'
+          > <span style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#e6e5e5;font-size:14px;">广公网安备44150202000124号</span></a></div>
+
     </div>
   </div>
 </template>
@@ -120,9 +155,7 @@ export default {
       else if (this.choosedSubject == 4) return this.subjectList2;
     }
   },
-  mounted() {
- 
-  },
+  mounted() {},
   methods: {
     changeSubject(subjectIndex) {
       this.$store.commit("setCurrentSubject", subjectIndex);
@@ -156,5 +189,16 @@ export default {
   text-align: -webkit-center;
   text-align: -moz-center;
   padding-top: 40px;
+}
+.footer {
+  margin-top: 100px;
+  min-height:182px;
+  padding:60px 0 50px;
+  background-color: #373d41;
+  .copyright {
+    color: #9fa7ad;
+    font-size: 14px;
+    margin-top: 10px;
+  }
 }
 </style>
